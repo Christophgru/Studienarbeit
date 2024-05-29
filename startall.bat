61eac2430eca00b11b3d0a8b432e54c5b3899592
Christophgru <christoph@familie-gruender.de>
2024-05-29 18:08:58 +0200

    startall bashscript compiles c++ and starts all processes. Might need to add some pip installs

--- startall.bat
+++ startall.bat
@@ -1,12 +1,32 @@
 @echo off
 
+REM Build the project using CMake
+echo Building the project...
+cd %SCRIPT_DIR%
+if not exist build (
+    mkdir build
+)
+cd build
+cmake ..
+cmake --build .
+
 set SCRIPT_DIR=%~dp0
 
 REM Start the C++ program in a new command prompt window
-start cmd.exe /k "%SCRIPT_DIR%/build/Studienarbeit.exe"
+start /min cmd.exe /k "%SCRIPT_DIR%/build/Studienarbeit.exe"
 
 REM Start the first Python script in a new command prompt window
-start cmd.exe /k "python.exe src/DataRetrievalAndUi/ui.py"
+start /min cmd.exe /k "python.exe %SCRIPT_DIR%/src/DataRetrievalAndUi/ui.py"
 
 REM Start the second Python script in a new command prompt window
-start cmd.exe /k "python.exe d:/Programmieren/Studienarbeit/src/DataRetrievalAndUi/getAnchorData.py"
+start /min cmd.exe /k "python.exe %SCRIPT_DIR%/src/DataRetrievalAndUi/getAnchorData.py"
+
+REM Keep the main window open and wait for user input to terminate
+echo Press Ctrl+C to terminate all processes
+:loop
+pause >nul
+goto loop
+
+REM Insert Function to terminate all processes here, we couldnt figure one out
+
+exit /b 0
