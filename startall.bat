2a680d144087f6a9147ecad10905f0ce1ceb4971
Christophgru <christoph@familie-gruender.de>
2024-05-29 19:01:27 +0200

    startall.bat should be working with normal Cmake setup

--- startall.bat
+++ startall.bat
@@ -7,13 +7,15 @@ if not exist build (
     mkdir build
 )
 cd build
-cmake ..
-cmake --build .
+cmake .. 
+REM if compalation doesn't work, try adapting command like following: cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=C:\mingw64\bin\gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:\mingw64\bin\g++.exe -G "MinGW Makefiles" ..
+cmake --build . 
+REM same here: cmake --build . --config Debug --target all -j 10
 
 set SCRIPT_DIR=%~dp0
 
 REM Start the C++ program in a new command prompt window
-start /min cmd.exe /k "%SCRIPT_DIR%/build/Studienarbeit.exe"
+start /min cmd.exe /k "%SCRIPT_DIR%\\build\\Studienarbeit.exe"
 
 REM Start the first Python script in a new command prompt window
 start /min cmd.exe /k "python.exe %SCRIPT_DIR%/src/DataRetrievalAndUi/ui.py"
