e58b9193fac7cd91978e4d1e203cf9a2909f0a83
Christophgru <christoph@familie-gruender.de>
2024-07-17 10:34:33 +0200

    Eigen include works on Windows

---
+++ src/Backend/kalman.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file kalman.cpp
+ * @brief 
+ * 
+ * 
+ * 
+ * @author Jonathan Simon
+ * @date 2024-05-17
+ */
+
+#include "kalman.h"
+#include <Eigen/Dense>
+
+calc::point KalmanFilter::filter(calc::point input){
+    //do crazy kalman shit here :)
+    if(history.size()==queueSize){
+        history.pop_back();
+        }
+    history.push_front(input);
+    calc::point output=input;
+    return output;
+};
+
+KalmanFilter::KalmanFilter(/* args */)
+{
+}
+
+KalmanFilter::~KalmanFilter()
+{
+}
